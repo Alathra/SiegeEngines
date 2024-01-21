892a8beadc9d5b61e58f6dc20d0e722f0f870048
C_Corp2002 <ccorp2002@gmail.com>
2024-01-21 17:36:11 -0500

    Fix Repiloting/Regaining Control

--- src/main/java/com/github/alathra/siegeengines/listeners/ClickHandler.java
+++ src/main/java/com/github/alathra/siegeengines/listeners/ClickHandler.java
@@ -1034,7 +1034,7 @@ public class ClickHandler implements Listener {
 		}
 		if (entity.getType() == EntityType.ARMOR_STAND) {
 			if (!(player.isSneaking()) && itemInHand.getType() == Config.controlItem) {
-				isSiegeEngine(entity, true);
+				if (isSiegeEngine(entity, false)) TakeControl(player, entity);
 				return;
 			}
 			if ((itemInHand.getType() == Material.AIR || itemInHand == null))  {
