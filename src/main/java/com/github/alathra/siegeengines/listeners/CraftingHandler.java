b80006e0951f86b9fac83eef5c4d208c4de9314a
ShermansWorld <77093412+ShermansWorld@users.noreply.github.com>
2024-01-27 22:57:07 -0500

    fix: crafting handler

--- src/main/java/com/github/alathra/siegeengines/listeners/CraftingHandler.java
+++ src/main/java/com/github/alathra/siegeengines/listeners/CraftingHandler.java
@@ -9,11 +9,13 @@ import org.bukkit.inventory.Recipe;
 import com.github.alathra.siegeengines.config.Config;
 import com.github.alathra.siegeengines.crafting.CraftingRecipes;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class CraftingHandler implements Listener {
 
 	@EventHandler
 	public void onCraft(CraftItemEvent event) {
-		if (!Config.craftingRecipes) {
+		if (Config.craftingRecipes) {
 			return;
 		}
 		Recipe recipe = event.getRecipe();
@@ -23,21 +25,33 @@ public class CraftingHandler implements Listener {
 		Player player = (Player) event.getView().getPlayer();
 		if (recipe.equals(CraftingRecipes.trebuchetRecipe())) {
 			if (!player.hasPermission("SiegeEngines.craft.trebuchet")) {
+				sendErrorMsg(player);
 				event.setCancelled(true);
+				return;
 			}
 		} else if (recipe.equals(CraftingRecipes.ballistaRecipe())) {
 			if (!player.hasPermission("SiegeEngines.craft.ballista")) {
+				sendErrorMsg(player);
 				event.setCancelled(true);
+				return;
 			}
 		} else if (recipe.equals(CraftingRecipes.swivelCannonRecipe())) {
 			if (!player.hasPermission("SiegeEngines.craft.swivel_cannon")) {
+				sendErrorMsg(player);
 				event.setCancelled(true);
+				return;
 			}
 		} else if (recipe.equals(CraftingRecipes.breachCannonRecipe())) {
 			if (!player.hasPermission("SiegeEngines.craft.breach_cannon")) {
+				sendErrorMsg(player);
 				event.setCancelled(true);
+				return;
 			}
 		}
+	}
+	
+	public void sendErrorMsg(Player player) {
+		player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cYou do not have permission to craft this siege engine"));
 	}
 
 }
